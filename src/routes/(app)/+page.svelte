0edf82f75fbe01a593cc7c3058e24cf0ae401015
Alejandro Sáez <alejandro.sapa@gmail.com>
2023-01-31 15:41:15 -0600

    fix README

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -15,20 +15,5 @@
 
 <div class="h-screen bg-blue-200">
   <h1 class="text-sm bg-red-500 text-black p-10">CONTENIDOs WEB</h1>
-  <div class="drawer">
-    <input id="my-drawer" type="checkbox" class="drawer-toggle" />
-    <div class="drawer-content">
-      <!-- Page content here -->
-      <label for="my-drawer" class="btn btn-primary drawer-button">Open drawer</label>
-    </div> 
-    <div class="drawer-side">
-      <label for="my-drawer" class="drawer-overlay"></label>
-      <ul class="menu p-4 w-80 bg-base-100 text-base-content">
-        <!-- Sidebar content here -->
-        <li><a>Sidebar Item 1</a></li>
-        <li><a>Sidebar Item 2</a></li>
-        
-      </ul>
-    </div>
-  </div>
+
 </div>
