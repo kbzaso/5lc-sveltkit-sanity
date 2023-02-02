b71100c8deffe8e83af14b8ea92d176f68a871ca
Alejandro Sáez <alejandro.sapa@gmail.com>
2023-02-02 14:22:14 -0600

    responsive to header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,7 +1,12 @@
-<div class="bg-[url('bg.png')] bg-cover h-screen">
-    <div class="absolute lg:bottom-32 lg:left-40 bottom-10 lg:w-1/2">
+
+
+<div class="bg-[url('bg.png')] bg-cover bg-center h-screen">
+    <div class="absolute bottom-0 bg-gradient-to-t from-black/100 via-black/60 to-transparent h-96 w-full">
+    </div>
+    <div class="container absolute lg:bottom-32 bottom-20 space-y-6 lg:space-y-0 left-[50%] px-4 -translate-x-[50%]">
         <h1 class="text-lg font-bold uppercase tracking-widest lg:-mb-4">Vive la experiencia ctm</h1>
-        <img src="h1.svg" alt="Siempre buena onda text">
-        <p class="text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut voluptates, enim, voluptate reprehenderit repudiandae ea nostrum illum necessitatibus iusto eius aliquam odit eveniet deserunt at eos, voluptas molestias laudantium culpa.|</p>
+        <img src="h1.svg" class="hidden md:block" alt="Siempre buena onda text">
+        <img src="sbo-mobile.svg" class="block md:hidden" alt="Siempre buena onda text">
+        <p class="text-lg lg:max-w-4xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ut voluptates, enim, voluptate reprehenderit repudiandae ea nostrum illum necessitatibus iusto eius aliquam odit eveniet deserunt at eos, voluptas molestias laudantium culpa.|</p>
     </div>
 </div>
