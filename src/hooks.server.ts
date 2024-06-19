e292601db2bc7bce957252179038ac1518599dc6
Alejandro Sáez <alejandro.sapa@gmail.com>
2024-06-18 20:09:53 -0400

    Add Sentry

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,14 @@
+import {sequence} from "@sveltejs/kit/hooks";
+import * as Sentry from "@sentry/sveltekit";
 import { getPreviewCookie } from "$lib/utils";
 import type { Handle } from "@sveltejs/kit";
 
-export const handle: Handle = async ({ event, resolve }) => {
+Sentry.init({
+    dsn: "https://4b8a94e1bafe01a1d7961c34cd6d19b9@o4507454758846464.ingest.us.sentry.io/4507455655575552",
+    tracesSampleRate: 1
+})
+
+export const handle: Handle = sequence(Sentry.sentryHandle(), async ({ event, resolve }) => {
   const previewModeCookie = getPreviewCookie(event.cookies);
 
   event.locals.previewMode = false;
@@ -13,4 +20,5 @@ export const handle: Handle = async ({ event, resolve }) => {
   const response = await resolve(event);
 
   return response;
-};
+});
+export const handleError = Sentry.handleErrorWithSentry();
