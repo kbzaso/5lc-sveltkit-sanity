3754f26f55a743c661d51bc357e38c6c7088a308
Alejandro Sáez <alejandro.sapa@gmail.com>
2024-08-12 18:40:57 -0400

    add differentiating points

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -16,13 +16,47 @@
     <div
       class="container absolute px-4 z-10 h-full flex flex-col justify-end items-center text-center bottom-20 md:bottom-0 md:justify-center left-[50%] -translate-x-[50%]"
     >
-      <h1 class="uppercase tracking-widest text-primary font-[AtomicMarker] text-7xl md:text-[150px] -rotate-3 drop-shadow-md">
+      <h1 class="uppercase tracking-widest text-primary font-[AtomicMarker] text-7xl md:text-[150px] -rotate-3 drop-shadow-md neon-flicker">
         {info.title}
       </h1>
 
-      <p class="text-lg text-white pt-4 max-w-xl">
+      <p class="text-lg text-white pt-4 max-w-xl z-10">
         {info.description}
       </p>
     </div>
     <img src={info.image} alt="Portada" class="object-cover md:object-cover absolute w-full h-full maskImages md:opacity-50">
   </header>
+
+
+  <style>
+.neon-flicker {
+    color: #fff;
+    text-shadow:
+      0 0 5px #fff,
+      0 0 10px #fff,
+      0 0 20px #fff,
+      0 0 40px #facc15,
+      0 0 80px #facc15,
+      0 0 90px #facc15,
+      0 0 100px #facc15,
+      0 0 110px #facc15;
+    animation: neon-flicker 1.5s infinite alternate;
+  }
+
+  @keyframes neon-flicker {
+    0%, 19%, 21%, 23%, 25%, 54%, 56%, 100% {
+      text-shadow:
+        0 0 5px #fff,
+        0 0 10px #fff,
+        0 0 20px #fff,
+        0 0 40px #facc15,
+        0 0 80px #facc15,
+        0 0 90px #facc15,
+        0 0 100px #facc15,
+        0 0 110px #facc15;
+    }
+    20%, 24%, 55% {
+      text-shadow: none;
+    }
+  }
+  </style>
--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -41,6 +41,13 @@
       currency: "CLP",
     }).format(thirdsTicketPrice);
   });
+
+
+  const info = {
+    SIEMPRE_BUENA_ONDA: 'https://res.cloudinary.com/dtj5xnlou/image/upload/f_auto,q_auto/v1/5LC/qnaz8aihcarkfaqjtipr',
+    INTENSIDAD: 'https://res.cloudinary.com/dtj5xnlou/image/upload/f_auto,q_auto/v1/5LC/mbyajlax7ea1hkuuwkbe',
+    BOVEDA_SECRETA: 'https://res.cloudinary.com/dtj5xnlou/image/upload/f_auto,q_auto/v1/5LC/bufh9o713u3otfclhnqq',
+  }
 </script>
 
 <svelte:head>
@@ -93,6 +100,25 @@
   <SuscribeForm />
   <!-- Puntos diferenciadores -->
   <section>
-
+    <div class="grid grid-cols-1 grid-rows-3 md:grid-cols-2 md:grid-rows-2 w-full lg:container mx-auto">
+      <div class="h-96 bg-[url('https://res.cloudinary.com/dtj5xnlou/image/upload/f_auto,q_auto/v1/5LC/qnaz8aihcarkfaqjtipr')] bg-cover relative bg-center">
+        <div class="text-7xl md:text-7xl lg:text-8xl text-primary h-fit absolute -bottom-20 w-full flex flex-col items-center z-10 mix-blend-plus-lighter">
+          <span class="font-[AtomicMarker] drop-shadow-lg -rotate-6">Siempre</span>
+          <span class="font-[AtomicMarker] drop-shadow-lg -rotate-12">Buena</span>
+          <spa class="font-[AtomicMarker] drop-shadow-lg -rotate-3">Onda</spa>
+        </div>
+      </div>
+      <div class="h-96 bg-[url('https://res.cloudinary.com/dtj5xnlou/image/upload/f_auto,q_auto/v1/5LC/mbyajlax7ea1hkuuwkbe')] bg-cover bg-center relative">
+        <div class="text-7xl md:text-7xl lg:text-8xl text-primary h-fit absolute -bottom-10 items-center w-full flex flex-col z-10 mix-blend-plus-lighter">
+          <span class="font-[AtomicMarker] drop-shadow-lg -rotate-6">Intensidad</span>
+        </div>
+      </div>
+      <div class="md:col-span-2 md:row-start-2 h-96 bg-[url('https://res.cloudinary.com/dtj5xnlou/image/upload/f_auto,q_auto/v1/5LC/bufh9o713u3otfclhnqq')] bg-cover bg-center relative">
+        <div class="text-7xl md:text-7xl lg:text-9xl text-primary h-fit absolute -bottom-16 w-full flex flex-col lg:flex-row items-center lg:justify-center">
+          <span class="font-[AtomicMarker] drop-shadow-lg mix-blend-plus-lighter -rotate-6">Bóveda</span>
+          <span class="font-[AtomicMarker] drop-shadow-lg mix-blend-plus-lighter -rotate-12">Secreta</span>
+        </div>
+      </div>
+    </div>
   </section>
 </div>
