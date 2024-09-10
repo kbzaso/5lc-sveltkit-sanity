f15b4b2858038aa703ff82b4d6f4f5000aabee4c
Alejandro Sáez <alejandro.sapa@gmail.com>
2024-09-10 14:57:07 -0300

    initial

--- src/lib/components/events/Cartelera.svelte
+++ src/lib/components/events/Cartelera.svelte
@@ -18,14 +18,41 @@
       >k</span
     >
   </h3>
-  <div id="catelera" class={`${billboard.length > 2 ? "columns-2 md:columns-3" : "columns-1 md:columns-2"} gap-4`}>
-  {#each billboard as image}
-      <img
-        class="w-full h-auto"
-        src={urlForImage(image).width(800).height(1000).url()}
-        alt={image.type_of_match}
-        title={image.type_of_match}
-      />
-      {/each}
-    </div>
+  <div
+    id="catelera"
+    class={`${
+      billboard.length > 2 ? "columns-2 md:columns-3" : "columns-1 md:columns-2"
+    } gap-4`}
+  >
+    {#each billboard as image, i}
+      <div>
+        <img
+          class="w-full h-auto"
+          src={urlForImage(image).width(800).height(1000).url()}
+          alt={image.type_of_match}
+          title={image.type_of_match}
+        />
+        <!-- Open the modal using ID.showModal() method -->
+        <button class="btn" onclick={`my_modal_${image.type_of_match.slice(0, 3)}_${i+1}.showModal()`}>open modal</button>
+        <dialog id={`my_modal_${image.type_of_match.slice(0, 3)}_${i+1}`} class="modal modal-bottom sm:modal-middle  bg-black/75">
+          <div class="modal-box">
+            <form method="dialog">
+              <button class="btn btn-sm btn-circle btn-primary absolute right-2 top-2">✕</button>
+            </form>
+            <h3 class="text-2xl font-ibm italic text-primary text-pretty">{image.type_of_match}</h3>
+            <p class="py-4 text-white">Press ESC key or click the button below to close</p>
+            <div class="modal-action">
+              <form method="dialog">
+                <!-- if there is a button in form, it will close the modal -->
+                <button class="btn">Close</button>
+              </form>
+            </div>
+          </div>
+          <form method="dialog" class="modal-backdrop">
+            <button>close</button>
+          </form>
+        </dialog>
+      </div>
+    {/each}
+  </div>
 </section>
