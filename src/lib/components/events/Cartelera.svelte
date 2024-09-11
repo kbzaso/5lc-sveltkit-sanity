8e3dec61c9fab35b3a668d305ba87979f0b37a20
Alejandro Sáez <alejandro.sapa@gmail.com>
2024-09-11 15:22:53 -0300

    stop video when dialog close

--- src/lib/components/events/Cartelera.svelte
+++ src/lib/components/events/Cartelera.svelte
@@ -1,9 +1,22 @@
 <script lang="ts">
   import { urlForImage } from "$lib/config/sanity";
-  import { TinySlider } from "svelte-tiny-slider";
+  import Youtube from "svelte-youtube-embed";
+  import { Youtube as YTIcon } from "lucide-svelte";
+  import { extractYouTubeId } from "$lib/utils";
 
   export let billboard;
   export let title: string = "Cartelera";
+
+  // const videoId = extractYouTubeId(billboard.images.url);
+  let reloadVideo = {};
+
+  // Function to reload the video
+  function reloadVideoComponent(dialogId) {
+    reloadVideo[dialogId] = false;
+    setTimeout(() => {
+      reloadVideo[dialogId] = true;
+    }, 0);
+  }
 </script>
 
 <section class="mt-10">
@@ -33,14 +46,52 @@
           title={image.type_of_match}
         />
         <!-- Open the modal using ID.showModal() method -->
-        <button class="btn" onclick={`my_modal_${image.type_of_match.slice(0, 3)}_${i+1}.showModal()`}>open modal</button>
-        <dialog id={`my_modal_${image.type_of_match.slice(0, 3)}_${i+1}`} class="modal modal-bottom sm:modal-middle  bg-black/75">
+        <button
+          on:click={() =>
+            reloadVideoComponent(
+              `my_modal_${image.type_of_match.slice(0, 3)}_${i + 1}`
+            )}
+          class="btn"
+          onclick={`my_modal_${image.type_of_match.slice(0, 3)}_${
+            i + 1
+          }.showModal()`}>open modal</button
+        >
+        <dialog
+          id={`my_modal_${image.type_of_match.slice(0, 3)}_${i + 1}`}
+          class="modal modal-bottom sm:modal-middle bg-black/75"
+          on:close={() =>
+            reloadVideoComponent(
+              `my_modal_${image.type_of_match.slice(0, 3)}_${i + 1}`
+            )}
+        >
           <div class="modal-box">
             <form method="dialog">
-              <button class="btn btn-sm btn-circle btn-primary absolute right-2 top-2">✕</button>
+              <button
+                class="btn btn-sm btn-circle btn-primary absolute right-2 top-2"
+                >✕</button
+              >
             </form>
-            <h3 class="text-2xl font-ibm italic text-primary text-pretty">{image.type_of_match}</h3>
-            <p class="py-4 text-white">Press ESC key or click the button below to close</p>
+            <h3 class="text-2xl font-ibm italic text-primary text-pretty">
+              {image.type_of_match}
+            </h3>
+            <p class="py-4 text-white">
+              Press ESC key or click the button below to close
+            </p>
+            {#if image.url}
+              {#await extractYouTubeId(image.url) then youtubeId}
+                {#if reloadVideo[`my_modal_${image.type_of_match.slice(0, 3)}_${i + 1}`]}
+                  <Youtube
+                    id={youtubeId}
+                    animations={false}
+                    --title-color={"transparent"}
+                  >
+                    <button class="bg-black/50 rounded-full p-4"
+                      ><YTIcon class="stroke-primary w-10 h-10" /></button
+                    >
+                  </Youtube>
+                {/if}
+              {/await}
+            {/if}
             <div class="modal-action">
               <form method="dialog">
                 <!-- if there is a button in form, it will close the modal -->
